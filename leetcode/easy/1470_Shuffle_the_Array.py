5a9068a44f94752a77ccb1a3beab32bbe5cf814a
shahrukhx01 <sk28671@gmail.com>
2022-08-09 14:20:57 +0200

    add shuffle array

---
+++ leetcode/easy/1470_Shuffle_the_Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+
+        n = len(nums) // 2
+
+        output = []
+        for i in range(n):
+            output.append(nums[i])
+            output.append(nums[i + n])
+
+        return output
