37e88c638503747305df19d988061ed30a4bb154
shahrukhx01 <sk28671@gmail.com>
2022-08-10 12:41:36 +0200

    add create target array in given order

---
+++ leetcode/easy/1389_Create_Target_Array_in_the_Given_Order.py
@@ -0,0 +1,7 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+
+        target = []
+        for idx in range(len(nums)):
+            target.insert(index[idx], nums[idx])
+        return target
