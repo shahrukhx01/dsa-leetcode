6f0d96059796c192a9ed89bf8715710bfb29d072
shahrukhx01 <sk28671@gmail.com>
2022-08-11 10:41:31 +0200

    add morse code words

---
+++ leetcode/easy/804_Unique_Morse_Code_Words.py
@@ -0,0 +1,68 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code = [
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--..",
+        ]
+        alphabets = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ]
+
+        morse_list = []
+        for word in words:
+            morse_word = ""
+            for alpha in word:
+                morse_word += morse_code[alphabets.index(alpha)]
+
+            morse_list.append(morse_word)
+
+        return len(set(morse_list))
